b75add27ec8f8ec4bed7562b1cd85e81e81828dd
sepzotto <sergiopatrique@gmail.com>
2025-07-11 17:19:09 -0300

    test: Criação das classes de teste unitário e teste de integração

---
+++ src/test/java/br/com/cotiinformatica/controllers/PedidosControllerTest.java
@@ -0,0 +1,44 @@
+package br.com.cotiinformatica.controllers;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PedidosControllerTest {
+	
+	@BeforeEach
+	public void setUp() {
+		// TODO preparar o ambiente de testes
+	}
+	@Test
+	@DisplayName("Deve executar POST /api/v1/pedidos com sucesso (201).")
+	public void testPostPedido_Sucesso() {
+		// TODO implementar o teste
+		fail("Não implementado.");
+	}
+	@Test
+	@DisplayName("Deve executar PUT /api/v1/pedidos com sucesso (200).")
+	public void testPutPedido_Sucesso() {
+		// TODO implementar o teste
+		fail("Não implementado.");
+	}
+	@Test
+	@DisplayName("Deve executar DELETE /api/v1/pedidos com sucesso (200).")
+	public void testDeletePedido_Sucesso() {
+		// TODO implementar o teste
+		fail("Não implementado.");
+	}
+	@Test
+	@DisplayName("Deve executar GET ALL /api/v1/pedidos com sucesso (200).")
+	public void testGetAllPedido_Sucesso() {
+		// TODO implementar o teste
+		fail("Não implementado.");
+	}
+	@Test
+	@DisplayName("Deve executar GET BY ID /api/v1/pedidos com sucesso (200).")
+	public void testGetByIdPedido_Sucesso() {
+		// TODO implementar o teste
+		fail("Não implementado.");
+	}
+}
+
---
+++ src/test/java/br/com/cotiinformatica/domain/PedidoServiceTest.java
@@ -0,0 +1,44 @@
+package br.com.cotiinformatica.domain;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PedidoServiceTest {
+	@BeforeEach
+	public void setUp() {
+		// TODO preparar o ambiente de testes
+	}
+	@Test
+	@DisplayName("Deve criar um pedido com sucesso.")
+	public void testCriarPedidoComSucesso() {
+		// TODO implementar o teste
+		fail("Não implementado.");
+	}
+	@Test
+	@DisplayName("Deve alterar os dados de um pedido existente com sucesso.")
+	public void testAlterarPedidoComSucesso() {
+		// TODO implementar o teste
+		fail("Não implementado.");
+	}
+	@Test
+	@DisplayName("Deve inativar um pedido existente com sucesso.")
+	public void testInativarPedidoComSucesso() {
+		// TODO implementar o teste
+		fail("Não implementado.");
+	}
+	@Test
+	@DisplayName("Deve consultar pedidos com sucesso.")
+	public void testConsultarPedidosComSucesso() {
+		// TODO implementar o teste
+		fail("Não implementado.");
+	}
+	@Test
+	@DisplayName("Deve obter um pedido através do ID com sucesso.")
+	public void testObterPedidoPorIdComSucesso() {
+		// TODO implementar o teste
+		fail("Não implementado.");
+	}
+}
+
+
