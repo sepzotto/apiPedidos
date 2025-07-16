40784c4197d8a627adc74943e77f2cf4cd86f639
sepzotto <sergiopatrique@gmail.com>
2025-07-16 17:34:38 -0300

    fix: Detalhamento da documentação do swagger

--- src/main/java/br/com/cotiinformatica/configurations/SwaggerConfiguration.java
+++ src/main/java/br/com/cotiinformatica/configurations/SwaggerConfiguration.java
@@ -1,25 +1,40 @@
 package br.com.cotiinformatica.configurations;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 @Configuration
 public class SwaggerConfiguration {
-	
 	@Bean
-	OpenAPI customApi() {
-		
-		var openApi = new OpenAPI()
-				.components(new Components())
-				.info(new Info()
-				.title("API Pedidos - Treinamento TJ/PR Turma 02")
-				.description("Curso Java Avançado - Formação Arquiteto COTI Informática")
-				.version("v1"));
-
-		return openApi;
+	OpenAPI customOpenApi() {
+		final String securitySchemeName = "bearerAuth";
+		return new OpenAPI()
+			.components(new Components()
+				.addSecuritySchemes(securitySchemeName,
+					new SecurityScheme()
+						.name(securitySchemeName)
+						.type(SecurityScheme.Type.HTTP)
+						.scheme("bearer")
+						.bearerFormat("JWT")))
+			.addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+			.info(new Info()
+				.title("API de Pedidos - Treinamento TJ/PR Turma 02")
+				.description("Projeto desenvolvido durante o curso **Java Avançado - Formação Arquiteto** da COTI Informática.")
+				.version("v1.0.0")
+				.termsOfService("https://www.cotiinformatica.com.br/termos")
+				.contact(new Contact()
+					.name("COTI Informática")
+					.email("contato@cotiinformatica.com.br")
+					.url("https://www.cotiinformatica.com.br"))
+				.license(new License()
+					.name("Licença MIT")
+					.url("https://opensource.org/licenses/MIT")));
 	}
-
 }
+
+
